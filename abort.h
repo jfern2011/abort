879a73a034f9f677b7f4d6e372170c5d220e636f
jfern2011 <jfern2011@gmail.com>
2018-12-13 23:41:18 -0800

    Fixed formatting

--- abort.h
+++ abort.h
@@ -16,75 +16,75 @@
 
 #define AbortIf_2(cond, ret)                           \
 {                                                      \
-	if (cond)                                          \
-	{                                                  \
-		char errMsg[256];                              \
+    if (cond)                                          \
+    {                                                  \
+        char errMsg[256];                              \
                                                        \
-		std::sprintf(errMsg, "[abort] %s in %s:%d",    \
-			         __PRETTY_FUNCTION__,              \
-			         __FILE__, __LINE__ );             \
+        std::sprintf(errMsg, "aborting %s in %s:%d",   \
+                     __PRETTY_FUNCTION__,              \
+                     __FILE__, __LINE__ );             \
                                                        \
-		if (errno)                                     \
-		{                                              \
-			std::perror(errMsg); std::fflush(stderr);  \
-			errno = 0;                                 \
-		}                                              \
-		else                                           \
-		{                                              \
-			std::printf("%s\n", errMsg);               \
-			std::fflush(stdout);                       \
-		}                                              \
-		return (ret);                                  \
-	}                                                  \
+        if (errno)                                     \
+        {                                              \
+            std::perror(errMsg); std::fflush(stderr);  \
+            errno = 0;                                 \
+        }                                              \
+        else                                           \
+        {                                              \
+            std::printf("%s\n", errMsg);               \
+            std::fflush(stdout);                       \
+        }                                              \
+        return (ret);                                  \
+    }                                                  \
 }
 
-#define AbortIf_3(cond, ret, msg, ...)                         \
-{                                                              \
-	if (cond)                                                  \
-	{                                                          \
-		char errMsg[256], fmt_msg[256];                        \
-		std::snprintf(fmt_msg, 256, msg, ##__VA_ARGS__);       \
-                                                               \
-		std::snprintf(errMsg, 256, "[abort] %s in %s:%d: %s",  \
-			         __PRETTY_FUNCTION__,                      \
-			         __FILE__, __LINE__,                       \
-			         fmt_msg);                                 \
-                                                               \
-		                                                       \
-		std::printf( "%s\n", errMsg ); std::fflush( stdout );  \
-		return (ret);                                          \
-	}                                                          \
+#define AbortIf_3(cond, ret, msg, ...)                          \
+{                                                               \
+    if (cond)                                                   \
+    {                                                           \
+        char errMsg[256], fmt_msg[256];                         \
+        std::snprintf(fmt_msg, 256, msg, ##__VA_ARGS__);        \
+                                                                \
+        std::snprintf(errMsg, 256, "aborting %s in %s:%d: %s",  \
+                     __PRETTY_FUNCTION__,                       \
+                     __FILE__, __LINE__,                        \
+                     fmt_msg);                                  \
+                                                                \
+                                                                \
+        std::printf( "%s\n", errMsg ); std::fflush( stdout );   \
+        return (ret);                                           \
+    }                                                           \
 }
 
 #define AbortIfNot_2(cond, ret)                \
-	AbortIf_2(!(cond), ret)
+    AbortIf_2(!(cond), ret)
 
 #define AbortIfNot_3(cond, ret, msg, ...)      \
-	AbortIf_3(!(cond), ret, msg, ##__VA_ARGS__)
+    AbortIf_3(!(cond), ret, msg, ##__VA_ARGS__)
 
 
 #define cat(a,b) a ## b
 #define _select(name, nargin)  cat( name ## _, nargin )
 
 #define get_nargin(_0 , _1,  _2 , _3 , _4 , _5 , _6 , _7 , \
-				   _8 , _9,  _10, _11, _12, _13, _14, _15, \
-				   _16, _17, _18, _19, _20, _21, _22, _23, \
-				   _24, _25, _26, _27, _28, _29, _30, _31, \
-				   _32, _33, _34, _35, _36, _37, _38, _39, \
-				   _40, _41, _42, _43, _44, _45, _46, _47, \
-				   _48, _49, _50, _51, _52, _53, _54, _55, \
-				   _56, _57, _58, _59, _60, _61, _62, _63, \
-				   nargin, ...) nargin
+                   _8 , _9,  _10, _11, _12, _13, _14, _15, \
+                   _16, _17, _18, _19, _20, _21, _22, _23, \
+                   _24, _25, _26, _27, _28, _29, _30, _31, \
+                   _32, _33, _34, _35, _36, _37, _38, _39, \
+                   _40, _41, _42, _43, _44, _45, _46, _47, \
+                   _48, _49, _50, _51, _52, _53, _54, _55, \
+                   _56, _57, _58, _59, _60, _61, _62, _63, \
+                   nargin, ...) nargin
 
 #define va_size(dummy, ...) get_nargin(0, ##__VA_ARGS__,    \
-									3, 3, 3, 3, 3, 3, 3, 3, \
-									3, 3, 3, 3, 3, 3, 3, 3, \
-									3, 3, 3, 3, 3, 3, 3, 3, \
-									3, 3, 3, 3, 3, 3, 3, 3, \
-									3, 3, 3, 3, 3, 3, 3, 3, \
-									3, 3, 3, 3, 3, 3, 3, 3, \
-									3, 3, 3, 3, 3, 3, 3, 3, \
-									3, 3, 3, 3, 3, 3, 3, 2)
+                                    3, 3, 3, 3, 3, 3, 3, 3, \
+                                    3, 3, 3, 3, 3, 3, 3, 3, \
+                                    3, 3, 3, 3, 3, 3, 3, 3, \
+                                    3, 3, 3, 3, 3, 3, 3, 3, \
+                                    3, 3, 3, 3, 3, 3, 3, 3, \
+                                    3, 3, 3, 3, 3, 3, 3, 3, \
+                                    3, 3, 3, 3, 3, 3, 3, 3, \
+                                    3, 3, 3, 3, 3, 3, 3, 2)
 
 #endif
 
@@ -97,8 +97,8 @@
  * to construct an error message
  */
 #define AbortIf(cond, ret, ...) \
-	_select(AbortIf, \
-		va_size(0, ##__VA_ARGS__))(cond, ret, ##__VA_ARGS__)
+    _select(AbortIf, \
+        va_size(0, ##__VA_ARGS__))(cond, ret, ##__VA_ARGS__)
 
 /**
  * @def Abort(ret, ...)
@@ -109,8 +109,8 @@
  * message
  */
 #define Abort(ret, ...) \
-	_select(AbortIf, \
-		va_size(0, ##__VA_ARGS__))(true, ret, ##__VA_ARGS__)
+    _select(AbortIf, \
+        va_size(0, ##__VA_ARGS__))(true, ret, ##__VA_ARGS__)
 
 /**
  * @def AbortIfNot(cond, ret, ...)
@@ -121,8 +121,8 @@
  * to construct an error message
  */
 #define AbortIfNot(cond, ret, ...) \
-	_select(AbortIfNot, \
-		va_size(0, ##__VA_ARGS__))(cond, ret, ##__VA_ARGS__)
+    _select(AbortIfNot, \
+        va_size(0, ##__VA_ARGS__))(cond, ret, ##__VA_ARGS__)
 
 
 #endif
